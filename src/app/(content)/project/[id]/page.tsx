6f0131056e92e62a912b07b4faa94fe29660b7e2
Sujan Naik <sujannaik047@gmail.com>
2025-04-18 15:39:34 +0100

    Maybe the MDX is null at build time ?

--- src/app/(content)/project/[id]/page.tsx
+++ src/app/(content)/project/[id]/page.tsx
@@ -38,7 +38,7 @@ export default async function Page({
 
 
     return (
-        <div></div>
+        <div><Project mdxComponent={project}/></div>
     )
 }
 
--- src/app/ui/projects/project.tsx
+++ src/app/ui/projects/project.tsx
@@ -1,32 +1,39 @@
-"use client";
-import {Suspense} from 'react';
-import {Transition} from "@headlessui/react";
-import LoadingCircleSpinner from "@/app/ui/components/Loading/LoadingCircleSpinner";
-import {MDXRemote, MDXRemoteSerializeResult} from "next-mdx-remote";
-import {MDXProvider} from "@mdx-js/react";
-import {useMDXComponents} from "../../../../mdx-components";
+"use client"
+import { Suspense } from "react"
+import { Transition } from "@headlessui/react"
+import LoadingCircleSpinner from "@/app/ui/components/Loading/LoadingCircleSpinner"
+import { MDXRemote, type MDXRemoteSerializeResult } from "next-mdx-remote"
+import { MDXProvider } from "@mdx-js/react"
+import { useMDXComponents } from "../../../../mdx-components"
 
 interface ProjectProps {
-    mdxComponent: MDXRemoteSerializeResult;
+  mdxComponent: MDXRemoteSerializeResult | null
 }
 
+const Project = ({ mdxComponent }: ProjectProps) => {
+  const components = useMDXComponents({})
 
-const Project = ({mdxComponent}: ProjectProps) => {
-    const components = useMDXComponents({});
-    return (
-        <Transition show={true} appear={true}>
+  // Add null checking to prevent errors during hydration
+  if (!mdxComponent || !mdxComponent.compiledSource) {
+    return <div>Content not available</div>
+  }
 
-            <div className={"transition duration-1000 ease-in data-[enter]:opacity-0"}>
-                <Suspense fallback={
-                    <LoadingCircleSpinner/>}>
-                    <MDXProvider>
-                        <MDXRemote compiledSource={mdxComponent.compiledSource} scope={mdxComponent.scope}
-                                   frontmatter={mdxComponent.frontmatter} components={components}></MDXRemote>
-                    </MDXProvider>
-                </Suspense>
-            </div>
-        </Transition>
-    );
-};
+  return (
+    <Transition show={true} appear={true}>
+      <div className={"transition duration-1000 ease-in data-[enter]:opacity-0"}>
+        <Suspense fallback={<LoadingCircleSpinner />}>
+          <MDXProvider>
+            <MDXRemote
+              compiledSource={mdxComponent.compiledSource}
+              scope={mdxComponent.scope || {}}
+              frontmatter={mdxComponent.frontmatter || {}}
+              components={components}
+            />
+          </MDXProvider>
+        </Suspense>
+      </div>
+    </Transition>
+  )
+}
 
-export default Project;
+export default Project
